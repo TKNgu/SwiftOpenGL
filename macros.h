45295ec05b057817510fcb44b3d886bbe0cca576
NgocPT <ngocpt@bkav.com>
2021-01-08 14:04:14 +0700

    Update GLAD.

--- macros.h
+++ macros.h
@@ -1,10 +1,2 @@
-#ifndef GL_GLEXT_PROTOTYPES
-#define GL_GLEXT_PROTOTYPES 1
-#endif
-
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#else
-#include <sys/types.h>
-#include <GL/gl.h>
-#endif
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
